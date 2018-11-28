11f01e58a72d788babacd1884d0c3cb7ead007f7
bitglimpse <bitglimpse@gmail.com>
2018-11-27 22:43:10 -0700

    Initial commit

---
+++ src/main/java/com/saasbass/lakeprofileservice/LakeProfileServiceApplication.java
@@ -0,0 +1,12 @@
+package com.saasbass.lakeprofileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakeProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LakeProfileServiceApplication.class, args);
+    }
+}
