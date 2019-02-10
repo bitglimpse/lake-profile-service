a062fc298919804da737a00464d2654e73301a84
Tyler <tyler@yourmom.com>
2019-02-10 10:13:24 -0700

    Cleanup

--- src/main/java/com/saasbass/lakeprofileservice/endpoint/Endpoint.java
+++ src/main/java/com/saasbass/lakeprofileservice/endpoint/Endpoint.java
@@ -22,8 +22,7 @@ public class Endpoint {
     private int getLakeProfileRequestCount = 0;
 
     @RequestMapping(value = "/{profileId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity getLakeProfile(@PathVariable Long profileId) throws InterruptedException, SocketException
-    {
+    public ResponseEntity getLakeProfile(@PathVariable Long profileId) throws SocketException {
         // Simulate a temporary socket exception caused by temporary server overload
         if (getLakeProfileRequestCount < 2) {
             getLakeProfileRequestCount++;
